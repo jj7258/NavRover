fe0dfc8168ad850c0206e6e25f389827a0e88b14
Joel Georgie Jacob <joelj7258@gmail.com>
2024-07-09 04:18:42 +0530

    Improved navigation and mapping parameters for 2D mapping

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,4 +7,7 @@
 # new environment variables
 
 # modified environment variables
+export LD_LIBRARY_PATH='/root/catkin_ws/devel/lib:/opt/ros/noetic/lib:/opt/ros/noetic/lib/x86_64-linux-gnu'
+export PKG_CONFIG_PATH='/root/catkin_ws/devel/lib/pkgconfig:/opt/ros/noetic/lib/pkgconfig:/opt/ros/noetic/lib/x86_64-linux-gnu/pkgconfig'
 export PWD='/root/NavRover/build'
+export PYTHONPATH='/opt/ros/noetic/lib/python3/dist-packages'
--- build/slam_gmapping/slam_gmapping/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/NavRover/src/slam_gmapping/slam_gmapping
-# Build directory: /root/NavRover/build/slam_gmapping/slam_gmapping
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
