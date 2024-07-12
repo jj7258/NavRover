a01935421fd3d3a1d5199e17951644c2a5c25fe7
Joel Georgie Jacob <43933982+jj7258@users.noreply.github.com>
2024-07-12 20:03:41 +0530

    Delete build directory

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/root/NavRover/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/root/NavRover/devel;/root/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/root/NavRover/devel;/root/catkin_ws/devel;/opt/ros/noetic' != '' else []
